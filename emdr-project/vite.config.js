66e9e26480b18dbd7c8f740ebdcac2b156cce7b9
PsychoZodiac <marcusghiasi@gmail.com>
2026-05-29 12:06:10 -0700

    Add SSR and SSG options to Vite config

--- emdr-project/vite.config.js
+++ emdr-project/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  ssr: {
+    noExternal: ['react-helmet-async'],
+  },
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+  },
 })
